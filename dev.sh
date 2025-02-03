8bdc741b8d25a1ab7581270f9e1b53d5d1140cb6
richard blundell <richardblundell@Mac.lan>
2025-02-03 18:57:07 -0500

    add basic test framework for osx

--- dev.sh
+++ dev.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
-nodemon -w . -V \
+nodemon -w . ${@:-} \
 	-i build -i .v -i boxes/generators/__pycache__ \
 	-e py,sh \
 		-x ./test.sh
--- test.sh
+++ test.sh
@@ -9,11 +9,23 @@ D1=10.0
 find boxes/generators -name "*.py"|xargs -I % python3 -m py_compile %
 
 [[ -d build ]] && mv build .b && rm -rf .b &
-pip install .||true
+pip install . &>/dev/null||true
 [[ -f t1.svg ]] && unlink t1.svg
-./scripts/boxes ABoxx \
+cmd="./scripts/boxes ABoxx \
 	--format svg \
 	--thickness $THICKNESS \
 	--d1 $D1 \
-	--output t1.svg
+	--output t1.svg"
+
+if ! eval "$cmd" | grep -q "OK2025"; then
+	echo -e "\n\nFailed to run boxes cmd \"$cmd\". Wanted OK2025 in output to validate.\n"
+	exit 1
+fi
+if [[ ! -f t1.svg ]]; then
+	echo -e "\n\nFailed to generate t1.svg!\n"
+	exit 1
+fi
+
+echo -e "BUILD OK"
+
 open -g t1.svg
